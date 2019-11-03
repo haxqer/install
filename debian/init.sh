8898796177f432ea3675b9046191dc6556153316
haxqer <2845513252@qq.com>
2019-11-03 16:35:08 +0800

    change init script

--- debian/init.sh
+++ debian/init.sh
@@ -11,6 +11,7 @@ apt update -y && \
         zsh \
         htop \
         curl \
+        vim \
         sudo
 
 haxqer_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2xwdBPeyJaJgddhu4cmYD2/s70Q/WeOrWqzEre3R90FTtjZtsBkx62oTiX+10gL/ZS3P4HGdkiHty2yV0SLJtpaP+ZCVu1aJyIoZrjHGAZNsJGD6ocPlnY47pmZERHxEebpQrJYzdUye2T7wIRZ+kkjHAcOkIclHQPanf/rVjpvQJ1yqaKS9zGHnEDFaptvHbDJQNS2NhhM0/NgLHHLxpIx4uHj6wMRdbkisiSyQhkDRne2SNE+TBV/w98vQIOff0n2wMlo8JV/kRdRBtBq35FOl0CiihNe5bhUlQWvqPgDqyUTJ9CARIk3+lwJjDPS5gB4Ba3N2j0HTYqW9XEN1r haxqer"
@@ -18,11 +19,13 @@ haxqer_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2xwdBPeyJaJgddhu4cmYD2/s70Q/W
 mkdir -p ~/.ssh
 echo ${haxqer_key} >> ~/.ssh/authorized_keys
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # docker
-./install-docker.sh
-./install-docker-compose.sh
+${DIR}/install-docker.sh
+${DIR}/install-docker-compose.sh
 
 
 #zsh
 #sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-./install-zsh.sh
+${DIR}/install-zsh.sh
