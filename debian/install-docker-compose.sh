eacf0c57df918030c244d679f6ed1098b2c9602b
haxqer <2845513252@qq.com>
2019-11-04 11:52:16 +0800

    docker compose download latest

--- debian/install-docker-compose.sh
+++ debian/install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
 && chmod +x /usr/local/bin/docker-compose \
 && docker-compose --version
