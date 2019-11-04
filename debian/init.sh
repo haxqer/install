fc1d64638ba19740ba9a748c643304fc08e7fa79
haxqer <2845513252@qq.com>
2019-11-04 11:11:27 +0800

    add some tools

--- debian/init.sh
+++ debian/init.sh
@@ -12,6 +12,10 @@ apt update -y && \
         htop \
         curl \
         vim \
+        conntrack \
+        ipvsadm \
+        ipset \
+        jq \
         sudo
 
 haxqer_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2xwdBPeyJaJgddhu4cmYD2/s70Q/WeOrWqzEre3R90FTtjZtsBkx62oTiX+10gL/ZS3P4HGdkiHty2yV0SLJtpaP+ZCVu1aJyIoZrjHGAZNsJGD6ocPlnY47pmZERHxEebpQrJYzdUye2T7wIRZ+kkjHAcOkIclHQPanf/rVjpvQJ1yqaKS9zGHnEDFaptvHbDJQNS2NhhM0/NgLHHLxpIx4uHj6wMRdbkisiSyQhkDRne2SNE+TBV/w98vQIOff0n2wMlo8JV/kRdRBtBq35FOl0CiihNe5bhUlQWvqPgDqyUTJ9CARIk3+lwJjDPS5gB4Ba3N2j0HTYqW9XEN1r haxqer"
