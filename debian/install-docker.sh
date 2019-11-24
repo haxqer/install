99f2639a723f048058b2308c7487560f34b2346d
haxqer <2845513252@qq.com>
2019-11-24 15:40:23 +0800

    change mirror

--- debian/install-docker.sh
+++ debian/install-docker.sh
@@ -9,7 +9,7 @@ apt-get update -y \
     software-properties-common \
 && curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -  \
 && add-apt-repository \
-   "deb [arch=x86_64] https://download.docker.com/linux/debian \
+   "deb [arch=x86_64] https://mirrors.aliyun.com/docker-ce/linux/debian \
    $(lsb_release -cs) \
    stable" \
 && apt-get update -y \
