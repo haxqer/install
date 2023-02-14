099b460665d0c49ef567ca5c20582465427c30d8
haxqer <2845513252@qq.com>
2023-02-14 21:44:17 +0800

    update docker

--- debian/install-docker.sh
+++ debian/install-docker.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 apt-get update -y \
-&& apt-get install -y \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release \
-&& curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
+&& apt-get install \
+       ca-certificates \
+       curl \
+       gnupg \
+       lsb-release -y \
+&& mkdir -m 0755 -p /etc/apt/keyrings \
+&& curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \
+&& chmod a+r /etc/apt/keyrings/docker.gpg \
 && echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null \
+     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
 && apt-get update -y \
-&& apt-get install -y docker-ce docker-ce-cli containerd.io \
+&& apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y \
 && docker run hello-world
+
