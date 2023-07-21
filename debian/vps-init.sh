1c7e563c4bb4951c707da10678f3971ca71cf7cc
haxqer <2845513252@qq.com>
2023-07-21 11:21:51 +0800

    change init.sh

--- debian/vps-init.sh
+++ debian/vps-init.sh
@@ -3,24 +3,21 @@
 # working directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-
-# install base tools
-${DIR}/install-base-tools.sh
-
-
 # add public key
 ${DIR}/add-pub-key.sh
 
+# install base tools
+${DIR}/install-base-tools.sh
 
 # docker
 ${DIR}/install-docker.sh
 ${DIR}/install-docker-compose.sh
 
-
 # zsh
 ${DIR}/install-zsh.sh
 
-
+# golang
+${DIR}/install-golang.sh
 
 # some images
 #${DIR}/pull-k8s-images.sh
