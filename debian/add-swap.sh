1e6dff76622b963cdebe8b00379067b4eb97a42b
haxqer <2845513252@qq.com>
2023-08-09 14:51:20 +0800

    fix bug

--- debian/add-swap.sh
+++ debian/add-swap.sh
@@ -4,4 +4,4 @@ cd /var
 dd if=/dev/zero of=swapfile bs=1024 count=1097152
 mkswap swapfile
 swapon swapfile
-echo '\n/var/swapfile        swap                 swap       defaults              0 0' >> /etc/fstab
+echo -e '\n/var/swapfile        swap                 swap       defaults              0 0\n' >> /etc/fstab
