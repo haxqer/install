6a794e3e47ad2ec251cda7a94d7145df65362f77
haxqer <2845513252@qq.com>
2023-08-15 14:03:17 +0800

    new v2ray install script

--- debian/install-v2ray.sh
+++ debian/install-v2ray.sh
@@ -4,5 +4,5 @@ apt update -y && \
     apt install -y \
         curl
 
-curl -Ls https://install.direct/go.sh | bash
-
+#curl -Ls https://install.direct/go.sh | bash
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
