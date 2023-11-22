aebf6f1cd190ba95c1e9dba8c56526429af9a150
haxqer <2845513252@qq.com>
2023-11-22 12:39:22 +0800

    add android

---
+++ android/install-base-tools.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+apt update -y && \
+    apt install -y \
+        htop \
+        build-essential \
+        net-tools \
+        dnsutils \
+        git \
+        tree \
+        zsh \
+        htop \
+        curl \
+        vim \
+        jq \
+        bash-completion \
+        unzip \
+        psmisc \
+        screen \
+        socat \
+        rsync \
+        nmap \
+        bmon \
+        iperf3 \
+        tmux \
+        gzip \
+        wget \
+        xclip
+
+        
+#        haproxy \
+
