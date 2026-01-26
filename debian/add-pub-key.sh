1babc0ac07480c911ca6078efb47d6db2d9abde4
haxqer <haxqer666@gmail.com>
2026-01-26 14:54:11 +0800

    change key

--- debian/add-pub-key.sh
+++ debian/add-pub-key.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-pub_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2xwdBPeyJaJgddhu4cmYD2/s70Q/WeOrWqzEre3R90FTtjZtsBkx62oTiX+10gL/ZS3P4HGdkiHty2yV0SLJtpaP+ZCVu1aJyIoZrjHGAZNsJGD6ocPlnY47pmZERHxEebpQrJYzdUye2T7wIRZ+kkjHAcOkIclHQPanf/rVjpvQJ1yqaKS9zGHnEDFaptvHbDJQNS2NhhM0/NgLHHLxpIx4uHj6wMRdbkisiSyQhkDRne2SNE+TBV/w98vQIOff0n2wMlo8JV/kRdRBtBq35FOl0CiihNe5bhUlQWvqPgDqyUTJ9CARIk3+lwJjDPS5gB4Ba3N2j0HTYqW9XEN1r haxqer"
+pub_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwzgqGNzBlRkdbFdBYIIw7EVRcycpVnOnfQ37YgSZh+ haxqer"
 
 mkdir -p ~/.ssh
 echo ${pub_key} >> ~/.ssh/authorized_keys
+
 
 
